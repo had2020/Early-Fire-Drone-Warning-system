ee61606c0904acee963b3548f1d8c6cb6e25aa74
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-01-21 15:31:55 -0800

    base files

---
+++ frontend/target/debug/deps/frontend-546e2ca71f0bf420.d
@@ -0,0 +1,5 @@
+/Users/hadrian/Developer/Github_Repos/Early-Fire-Drone-Warning-system/frontend/target/debug/deps/libfrontend-546e2ca71f0bf420.rmeta: src/main.rs
+
+/Users/hadrian/Developer/Github_Repos/Early-Fire-Drone-Warning-system/frontend/target/debug/deps/frontend-546e2ca71f0bf420.d: src/main.rs
+
+src/main.rs:
---
+++ frontend/target/debug/deps/frontend-81d428ee71b53e56.d
@@ -0,0 +1,5 @@
+/Users/hadrian/Developer/Github_Repos/Early-Fire-Drone-Warning-system/frontend/target/debug/deps/libfrontend-81d428ee71b53e56.rmeta: src/main.rs
+
+/Users/hadrian/Developer/Github_Repos/Early-Fire-Drone-Warning-system/frontend/target/debug/deps/frontend-81d428ee71b53e56.d: src/main.rs
+
+src/main.rs:
